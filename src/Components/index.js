685421301bec679d7405e15dba207a8c1313b89f
David Röger <david@codeatelier.com>
2021-06-30 15:54:02 +0200

    Format remaining

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,4 @@
-export { Nav } from './Nav/Nav'
-export { DeviceElement } from './DeviceElement/DeviceElement'
-export { StateElement } from './StateElement/StateElement'
-export { StateElementCard } from './StateElementCard/StateElementCard'
+export { Nav } from "./Nav/Nav";
+export { DeviceElement } from "./DeviceElement/DeviceElement";
+export { StateElement } from "./StateElement/StateElement";
+export { StateElementCard } from "./StateElementCard/StateElementCard";
